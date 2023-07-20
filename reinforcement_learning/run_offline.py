078651d8670d6bef28b56a49888912aa76d15d1b
Tianyi Miao <tennielmiao@gmail.com>
2023-07-20 19:10:16 -0400

    modify offline script

--- reinforcement_learning/run_offline.py
+++ reinforcement_learning/run_offline.py
@@ -46,26 +46,6 @@ def gae(rewards: np.ndarray, critic_values: np.ndarray, gamma: float, gae_lambda
     return advantages
 
 
-# def gae_advantages(rewards: torch.Tensor, critic_values: torch.Tensor, gamma: float, gae_lambda: float):
-#     # Single trajectory generalized advantage estimation
-#     # Without gradients, compute a list of advantage based on
-#     # estimated values and rewards per timestep.
-#     # rewards, critic_values should have the same length n (the last terminal state is excluded)
-#     # it's better to calculate the entire thing in CPU
-#     with torch.no_grad():
-#         n_steps = len(rewards)
-#         old_device = rewards.device
-#         # rewards = torch.tensor(rewards, device="cpu")
-#         rewards = rewards.detach().clone().cpu()
-#         critic_values = critic_values.detach().clone().cpu()
-#         advantages = torch.zeros(n_steps, device="cpu")
-#         advantages[-1] = rewards[-1] - critic_values[-1]
-#         for t in reversed(range(n_steps-1)):    # from n_steps-2 to 0 inclusive
-#             delta = rewards[t] + gamma * critic_values[t + 1] - critic_values[t]
-#             advantages[t] = delta + gamma * gae_lambda * advantages[t + 1]
-#         return advantages.to(device=old_device)
-
-
 class VPG:
     # vanilla policy gradient
     def __init__(self, state_dim, action_dim, gamma=0.9, learning_rate=0.01, batch_size=32):
@@ -87,7 +67,6 @@ class VPG:
             nn.Linear(32, 32),
             nn.ReLU(),
             nn.Linear(32, self.action_dim),
-            # nn.Softmax(dim=-1)
         )
         return policy_net
 
@@ -159,35 +138,9 @@ class VPG:
             self.optimizer.step()
             self.optimizer.zero_grad()
 
-    # def learn(self, episodes):
-    #     for i, (obs_list, act_list, reward_list) in enumerate(episodes):
-    #         obs_list = np.array(obs_list)
-    #         act_list = np.array(act_list)
-    #         reward_list = np.array(reward_list)
-    #         obs_tensor = torch.tensor(obs_list, dtype=torch.float32)
-    #         act_tensor = torch.tensor(act_list, dtype=torch.long)
-    #         reward_tensor = torch.tensor(reward_list, dtype=torch.float32)
-    #
-    #         adv_tensor = gae_advantages(rewards=reward_tensor, critic_values=torch.zeros_like(reward_tensor), gamma=self.gamma, gae_lambda=1.0)
-    #
-    #         dataset = TensorDataset(obs_tensor, act_tensor, adv_tensor)
-    #         dataloader = DataLoader(dataset, batch_size=self.batch_size, shuffle=True)
-    #
-    #         for obs_batch, act_batch, reward_batch in dataloader:
-    #             action_logits = self.policy_net(obs_batch)
-    #             log_probs = Categorical(logits=action_logits).log_prob(act_batch)
-    #             # action_probs = self.policy_net(obs_batch)  # [batch_size, 1]
-    #             # log_probs = Categorical(action_probs).log_prob(act_batch)
-    #             # log_probs = torch.log(torch.gather(action_probs, 1, act_batch.unsqueeze(1))).squeeze()
-    #             # print((log_probs_other - log_probs).abs().mean())
-    #             policy_loss = -(log_probs * reward_batch).mean()
-    #
-    #             policy_loss.backward()
-    #             self.optimizer.step()
-    #             self.optimizer.zero_grad()
-
 
 def run_offline(env, agent, episodes_per_learn=1000, max_frames=100000):
+    episode_count = 0
     last_n_rewards = []
 
     episodes = []
@@ -203,6 +156,7 @@ def run_offline(env, agent, episodes_per_learn=1000, max_frames=100000):
         obs = next_obs
         if terminated or truncated:
             total_reward = sum(reward_list)
+            episode_count += 1
 
             episodes.append((obs_list, act_list, reward_list))
             obs_list, act_list, reward_list = [], [], []
@@ -211,6 +165,7 @@ def run_offline(env, agent, episodes_per_learn=1000, max_frames=100000):
             n = min(30, len(last_n_rewards))
             avg = sum(last_n_rewards[-n:]) / n
             improvement_emoji = "🔥" if (total_reward > avg) else "😢"
+            improvement_emoji = f"{episode_count:3} " + improvement_emoji
             print(
                 f"{improvement_emoji} Finished with reward {int(total_reward)}.\tAverage of last {n}: {int(avg)}"
             )
@@ -225,18 +180,21 @@ def run_lunar_lander(visualize=True):
     agent = VPG(state_dim=env.observation_space.shape[0],
                 action_dim=env.action_space.n,
                 gamma=0.95,
-                learning_rate=0.001,
-                batch_size=32)
+                learning_rate=0.003,
+                batch_size=64)
 
-    run_offline(env, agent, episodes_per_learn=5, max_frames=50_000)
+    run_offline(env, agent, episodes_per_learn=5, max_frames=150_000)
 
 
 def run_cart_pole(visualize=True):
     env = gym.make("CartPole-v1", render_mode="human" if visualize else None)
+    # env = gym.make("MountainCar-v0", render_mode="human" if visualize else None)
+    # env = gym.make("Acrobot-v1", render_mode="human" if visualize else None)
+    # env = gym.make("Pendulum-v1", render_mode="human" if visualize else None)
 
     agent = VPG(state_dim=env.observation_space.shape[0],
                 action_dim=env.action_space.n,
-                gamma=1.0,
+                gamma=0.95,
                 learning_rate=3e-3,
                 batch_size=128)
 
