5d1964ee90f4fb583b7a07729d953f04a5a54d4d
Tianyi Miao <tennielmiao@gmail.com>
2023-07-21 11:30:34 -0400

    add plotting and last-value bootstrapping

--- reinforcement_learning/run_offline.py
+++ reinforcement_learning/run_offline.py
@@ -155,6 +155,10 @@ def run_offline(env, agent, episodes_per_learn=1000, max_frames=100000):
         reward_list.append(reward)
         obs = next_obs
         if terminated or truncated:
+            # Last-value bootstrapping actually hurts performance in CartPole,
+            # so it's disabled by default.
+            # if truncated:
+            #     obs_list.append(obs)
             total_reward = sum(reward_list)
             episode_count += 1
 
@@ -172,6 +176,11 @@ def run_offline(env, agent, episodes_per_learn=1000, max_frames=100000):
             if len(episodes) >= episodes_per_learn:
                 agent.learn(episodes)
                 episodes = []
+
+    from matplotlib import pyplot as plt
+    plt.scatter(np.arange(len(last_n_rewards)) // episodes_per_learn * episodes_per_learn,
+                last_n_rewards)
+    plt.show()
 
 
 def run_lunar_lander(visualize=True):
