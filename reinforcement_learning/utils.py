e8738752904835db56c4c3d6e3e6cc636ee63970
Tianyi Miao <tennielmiao@gmail.com>
2023-07-21 11:30:45 -0400

    add last-value bootstrapping

--- reinforcement_learning/utils.py
+++ reinforcement_learning/utils.py
@@ -13,8 +13,10 @@ def gae(rewards: np.ndarray, critic_values: np.ndarray, gamma: float, gae_lambda
     n_steps = len(rewards)
     advantages = np.zeros(n_steps)
     advantages[-1] = rewards[-1] - critic_values[n_steps-1]
-    # if len(critic_values) == n_steps + 1:   # truncated
-    #     advantages[-1] += gamma * critic_values[n_steps]
+    # Weirdly, last-value bootstrapping for truncated episodes
+    # actually hurts performance in CartPole.
+    if len(critic_values) == n_steps + 1:   # truncated
+        advantages[-1] += gamma * critic_values[n_steps]
     for t in reversed(range(n_steps - 1)):  # from n_steps-2 to 0 inclusive
         delta = rewards[t] + gamma * critic_values[t + 1] - critic_values[t]
         advantages[t] = delta + gamma * gae_lambda * advantages[t + 1]
